2c81f1034bee99944caea379999adfedbe48f647
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-08 05:29:05 +0100

    chore : set up training data for  chatbot

---
+++ features/chat/utils/train.js
@@ -0,0 +1,11 @@
+const { NlpManager } = require('node-nlp');
+const nplConfiguration = { nlu: { log: true }, languages: ['en'] };
+const manager = new NlpManager(nplConfiguration);
+
+async function trainAI(str) {
+  await manager.addCorpus('./tset-en.json');
+  await nlp.train();
+  return await manager.process('en', str);
+}
+
+module.exports = trainAI;
