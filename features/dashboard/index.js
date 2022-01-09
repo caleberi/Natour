a5f3dc15208580b12e636bf4f66b38083d8a5fd1
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-09 20:25:13 +0100

    added a simple dashboard backend

--- features/dashboard/index.js
+++ features/dashboard/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const {
+  getDashboardUsers,
+  getDashboardTours,
+  getDashboardReviews,
+} = require('../../controllers/dashboardController');
+
+const { roles } = require('../../helpers/constants');
+const { isAuthenticated, isAuthorized } = require('../../middlewares');
 const router = express.Router({
   mergeParams: true,
   caseSensitive: true,
 });
 
 module.exports = (io) => {
+  router.use(isAuthenticated, isAuthorized(roles.C_ADMIN));
+  router.get('/users', getDashboardUsers);
+  router.get('/tours', getDashboardTours);
+  router.get('/reviews', getDashboardReviews);
   return router;
 };
