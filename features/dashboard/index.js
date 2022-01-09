8787e328a64db58d7510a8d3ee6c03cfad6fe4a1
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-09 22:16:59 +0100

    added auth to dashboard

--- features/dashboard/index.js
+++ features/dashboard/index.js
@@ -5,7 +5,7 @@ const {
   getDashboardReviews,
 } = require('../../controllers/dashboardController');
 
-const { roles } = require('../../helpers/constants');
+const { roles, messages } = require('../../helpers/constants');
 const { isAuthenticated, isAuthorized } = require('../../middlewares');
 const router = express.Router({
   mergeParams: true,
