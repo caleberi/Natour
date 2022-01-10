90aa337074443f2b197ac51c5df8e5de7a6177c9
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-10 16:36:20 +0100

    added data layer for template files

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,17 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', {
+const tourDB = require('../model/tourModel');
+const { codes } = require('../helpers/constants');
+exports.getOverview = async (req, res) => {
+  const tours = await tourDB.find();
+  return res.status(codes.OK).render('overview', {
     title: 'All Tours',
+    tours,
   });
 };
 
-exports.getTour = (req, res) => {
+exports.getTour = async (req, res) => {
+  const tour = await tourDB.findOne({ slug: req.params.slug });
   res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+    title: tour.name,
+    tour,
   });
 };
