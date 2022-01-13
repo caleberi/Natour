869ad3db5416f757b54362dce29db97901dd44f8
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-13 21:59:23 +0100

    added tour check

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,6 +5,11 @@ const { AppError, ValidationError } = require('../helpers/error');
 
 exports.getOverview = async (req, res) => {
   const tours = await tourDB.find();
+  if (!tours) {
+    return next(
+      new AppError(messages.NO_ENTITY('Tours'), codes.NOT_FOUND, false)
+    );
+  }
   return res.status(codes.OK).render('overview', {
     title: 'All Tours',
     tours,
