e05b1e72b27ebd9ee09b2f741806cba40f1d0b1e
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-13 22:00:51 +0100

    added logic for rendering  pages

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -31,3 +31,64 @@ exports.getTour = async (req, res) => {
     tour,
   });
 };
+
+exports.getLoginForm = async (req, res) => {
+  res.status(codes.OK).render('login', {
+    title: ' Log into your account',
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(codes.OK).render('account', {
+    title: 'My account',
+  });
+};
+
+exports.updateUserData = async (req, res, next) => {
+  let user = await userDB.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      photo: req.file.filename,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  if (!user) {
+    return next(
+      new AppError(
+        messages.NO_ENTITY(`User : ${req.body.name}`),
+        codes.NOT_FOUND,
+        false
+      )
+    );
+  }
+  res.status(codes.OK).render('account', {
+    title: 'My account',
+    user: user,
+  });
+};
+
+exports.updateUserPassword = async (req, res, next) => {
+  const user = await userDB.findById(req.user.id).select('+password');
+  const { oldPassword, newPassword, newPasswordConfirm } = req.body;
+  if (!(await user.verifyPassword(oldPassword, user.password))) {
+    return next(
+      new ValidationError(
+        messages.INCORRECT_OLD_PASSWORD,
+        codes.UNAUTHORIZED,
+        false
+      )
+    );
+  }
+  user.password = newPassword;
+  user.passwordConfirm = newPasswordConfirm;
+  await user.save();
+  res.status(codes.OK).render('account', {
+    title: 'My account',
+    user: user,
+  });
+};
