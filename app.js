c0ce996c2d7c0cf18161335bffb1040585a76fed
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-13 22:13:11 +0100

    added new update

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const config = require('./config');
 const path = require('path');
-const { messages, tools, environments } = require('./helpers/constants');
+const cookieParser = require('cookie-parser');
+const { messages, tools, environments, codes } = require('./helpers/constants');
 const apiLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   max: 100,
@@ -28,17 +29,51 @@ if (_.isEqual(config.currentEnviroment, environments.DEVELOPMENT)) {
 } else {
   app.use(morgan(tools.LOGGER_TEMPLATE));
 }
-app.use(cors());
-app.use(helmet());
+const CSP = 'Content-Security-Policy';
+const POLICY =
+  // "default-src 'self' https://*.mapbox.com;" +
+  "base-uri 'self';block-all-mixed-content;" +
+  "font-src 'self' https: data:;" +
+  "frame-ancestors 'self';" +
+  "img-src http://localhost:3000 'self' blob: data:;" +
+  "object-src 'none';" +
+  "script-src https: cdn.jsdelivr.net cdnjs.cloudflare.com api.mapbox.com 'self' blob: ;" +
+  "script-src-attr 'none';" +
+  "style-src 'self' https: 'unsafe-inline';" +
+  'upgrade-insecure-requests;';
+app.use((req, res, next) => {
+  res.setHeader(CSP, POLICY);
+  res.setHeader('Access-Control-Allow-Origin', ' *');
+  next();
+});
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000/*',
+    optionsSuccessStatus: codes.OK,
+  })
+);
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ limit: '100kb', extended: true }));
+app.use(cookieParser());
+app.use((req, res, next) => {
+  req.requestedTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp({ whitelist: tools.HPP_WHITELIST }));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './views'));
 app.locals.moment = require('moment');
-app.get('/ping', (req, res, next) => res.send('Pinged'));
-app.use('/public', express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')));
 app.use(`/api/v1`, apiLimiter);
 app.use(`/api/v1/tours`, tourRouter);
 app.use(`/api/v1/users`, userRouter);
