b3744ebaef4eff4e87c845422c352caea932cdf2
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-28 22:31:41 +0100

    feat :  add user db model

--- model/userModel.js
+++ model/userModel.js
@@ -36,10 +36,15 @@ const userSchema = mongoose.Schema(
     password: {
       type: String,
       required: [true, 'Please provide a password'],
-      validate: [
-        validator.isStrongPassword,
-        'Please provide a strong password',
-      ],
+      validate: {
+        validator: (password) => {
+          const re = new RegExp(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})'
+          );
+          return re.test(password);
+        },
+        message: 'Please provide a strong password',
+      },
       minlength: 8,
       selected: false,
     },
