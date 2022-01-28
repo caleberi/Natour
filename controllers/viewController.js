969a9f383c10e9eb6393db8baf28a719f75a4fa1
Caleb Adewole <caleberioluwa@gmail.com>
2022-01-28 22:26:16 +0100

    feat :  add user view for signup login ,otp etc

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,7 @@
 const tourDB = require('../model/tourModel');
 const userDB = require('../model/userModel');
+const bookingDB = require('../model/bookingModel');
+const reviewDB = require('../model/reviewModel');
 const { codes, messages } = require('../helpers/constants');
 const { AppError, ValidationError } = require('../helpers/error');
 
@@ -10,7 +12,7 @@ exports.getOverview = async (req, res) => {
       new AppError(messages.NO_ENTITY('Tours'), codes.NOT_FOUND, false)
     );
   }
-  return res.status(codes.OK).render('overview', {
+  res.status(codes.OK).render('overview', {
     title: 'All Tours',
     tours,
   });
@@ -21,6 +23,10 @@ exports.getTour = async (req, res) => {
     path: 'reviews',
     field: 'review rating user',
   });
+  const booked = await bookingDB.findOne({
+    user: res.locals.user,
+    tour: tour.id,
+  });
   if (!tour) {
     return next(
       new AppError(messages.NO_ENTITY(req.params.slug), codes.NOT_FOUND, false)
@@ -29,6 +35,7 @@ exports.getTour = async (req, res) => {
   res.status(codes.OK).render('tour', {
     title: tour.name,
     tour,
+    booked,
   });
 };
 
@@ -91,4 +98,87 @@ exports.updateUserPassword = async (req, res, next) => {
     title: 'My account',
     user: user,
   });
+};
+
+exports.getSignupForm = (req, res, next) => {
+  res.status(codes.OK).render('signup', {
+    title: 'Register',
+  });
+};
+
+exports.getMyTour = async (req, res, next) => {
+  const bookings = await bookingDB.find({ user: req.user.id });
+  const tourIDs = bookings.map((booking) => booking.tour);
+  const tours = await tourDB.find({ _id: { $in: tourIDs } });
+  res.status(codes.OK).render('overview', {
+    title: 'My Bookings',
+    tours,
+  });
+};
+
+exports.getManageTour = async (req, res, next) => {
+  res.status(codes.OK).render('manage_tour', {
+    title: 'Manage Tour',
+  });
+};
+
+exports.getForgetPasswordPage = async (req, res, next) => {
+  res.status(codes.OK).render('forgot', {
+    title: 'Forgot Password',
+  });
+};
+
+exports.getResetPasswordPage = async (req, res, next) => {
+  res.status(codes.OK).render('reset_password', {
+    title: 'Reset Password',
+    token: req.params.token,
+  });
+};
+
+exports.getForgetPasswordOTPPage = async (req, res, next) => {
+  res.status(codes.OK).render('otp', {
+    title: 'Reset Password With OTP',
+  });
+};
+
+exports.createReview = async (req, res) => {
+  const userId = req.user.id;
+  const tourId = (await tourDB.findOne({ slug: req.params.slug })).id;
+  const previousReview = await reviewDB.findOne({ user: userId, tour: tourId });
+  if (previousReview) {
+    await reviewDB.deleteOne({ user: userId, tour: tourId });
+  }
+  await reviewDB.create({
+    user: userId,
+    tour: tourId,
+    ...req.body,
+  });
+  res.redirect(`/tours/${req.params.slug}`);
+};
+
+exports.likeOrUnlikeTour = async (req, res) => {
+  const { tourSlug, userId } = req.params;
+  if (
+    await tourDB.findOne({
+      likeCount: userId,
+    })
+  ) {
+    await tourDB.findOneAndUpdate(
+      { slug: tourSlug },
+      {
+        $pull: {
+          likeCount: userId,
+        },
+      }
+    );
+  }
+  await tourDB.findOneAndUpdate(
+    { slug: tourSlug },
+    {
+      $push: {
+        likeCount: userId,
+      },
+    }
+  );
+  res.redirect(`/tours/${tourSlug}`);
 };
